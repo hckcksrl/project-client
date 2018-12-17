552f8d9c108290a0b5dc3a23f82faa0ad3df7256
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-17 18:01:13 +0900

    Apollo

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,42 @@
-import { ApolloClient } from "apollo-client";
-import { InMemoryCache } from "apollo-boost";
+import ApolloClient from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { withClientState } from "apollo-link-state";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-boost";
 
 const cache = new InMemoryCache();
 
-const client = new ApolloClient({
-  cache
+const getToken = () => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    return token;
+  } else {
+    return null;
+  }
+};
+
+const auth = new ApolloLink((operation, next) => {
+  operation.setContext({
+    header: {
+      "X-JWT": getToken()
+    }
+  });
+  return next(operation);
 });
+
+const http = new HttpLink({
+  uri: "http://localhost:4000/graphql"
+});
+
+const state = withClientState({
+  cache
+  //   defaults,
+  //   resolvers
+});
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([auth, state, http])
+});
+
+export default client;
