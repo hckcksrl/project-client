aaf4ac33254a2aadda0646a4eb10da2c15f8aa9a
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-18 16:58:44 +0900

    Login Queries

---
+++ src/Route/User/Login/queries.js
@@ -0,0 +1,11 @@
+import gql from "graphql-tag";
+
+export const EmailLogin = gql`
+  mutation Email_Login($email: String!, $password: String!) {
+    Login(email: $email, password: $password) {
+      result
+      error
+      token
+    }
+  }
+`;
