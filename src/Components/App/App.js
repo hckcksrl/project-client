963245ed3d4acae951dd8040da505d5252723759
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-20 22:01:20 +0900

    App Route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import StartApp from "./StartApp";
-import IsLogin from "../../Route/User/Login";
+import Regist from "../../Route/User/Register";
+import Login from "../../Route/User/Login";
 
 class App extends Component {
   render() {
@@ -9,7 +10,8 @@ class App extends Component {
       <BrowserRouter>
         <Switch>
           <Route exact={true} path={"/"} component={StartApp} />
-          <Route path={"/login"} component={IsLogin} />
+          <Route path={"/login"} component={Login} />
+          <Route path={"/regist"} component={Regist} />
         </Switch>
       </BrowserRouter>
     );
