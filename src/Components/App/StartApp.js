483f969ddc24ec77f637de31c19428934294baa7
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-20 16:20:23 +0900

    Start App Page

--- src/Components/App/StartApp.js
+++ src/Components/App/StartApp.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class StartApp extends Component {
   render() {
-    return <div>aaa</div>;
+    return <Link to={"/login"}>a</Link>;
   }
+  _click = () => {
+    this.render("/login");
+  };
 }
 
 export default StartApp;
