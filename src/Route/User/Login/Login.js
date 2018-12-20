48737d8ea9f6920c528eb7a0972f6c41cdcde8dc
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-20 16:20:32 +0900

    Login Page And Mutation

--- src/Route/User/Login/Login.js
+++ src/Route/User/Login/Login.js
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { EmailLogin } from "./queries";
+import { Mutation, MutationFn } from "react-apollo";
+import bcrypt from "bcryptjs";
 
 const Input = styled.input`
   padding: 1em;
@@ -28,6 +31,8 @@ const Button = styled.button`
   border-radius: 3px;
 `;
 
+const Form = styled.form``;
+
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
@@ -37,18 +42,61 @@ export default class Login extends React.Component {
     };
   }
   render() {
+    const { history } = this.props;
     const { email, password } = this.state;
     return (
-      <>
-        <Input placeholder="email" type="email" ref={email} />
-        <Password placeholder="password" type="password" ref={password} />
-        <Button onClick="_Login">Login</Button>
-      </>
+      <Mutation
+        mutation={EmailLogin}
+        variables={{ email: email, password: password }}
+        onCompleted={data => {
+          console.log(data);
+          const { Login } = data;
+          if (Login.result) {
+            localStorage.setItem("token", Login.token);
+            history.push("/");
+          } else {
+            console.log(Login.error);
+          }
+        }}
+      >
+        {Email_Login => (
+          <Form
+            onSubmit={e => {
+              e.preventDefault();
+              Email_Login({
+                variables: {
+                  email: email,
+                  password: password
+                }
+              });
+            }}
+          >
+            <Input
+              placeholder="email"
+              type="email"
+              onChange={this._onInputChange}
+              value={email}
+              name={"email"}
+            />
+            <Password
+              placeholder="password"
+              type="password"
+              onChange={this._onInputChange}
+              value={password}
+              name={"password"}
+            />
+            <Button type="submit">Login</Button>
+          </Form>
+        )}
+      </Mutation>
     );
   }
-  _Login = () => {
-    const { Login } = this.props;
-    const { email, password } = this.state;
-    Login(email, password);
+  _onInputChange = event => {
+    const {
+      target: { value, name }
+    } = event;
+    this.setState({
+      [name]: value
+    });
   };
 }
