9ab82fdc3dbdb28c1df8daa4841f21f88d5562f5
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-25 19:41:24 +0900

    App Route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,7 @@ import StartApp from "./StartApp";
 import Regist from "../../Route/User/Register";
 import Login from "../../Route/User/Login";
 import CreateProjects from "../../Route/Project/CreateProject";
+import LogOut from "../../Route/User/Logout";
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Route path={"/login"} component={Login} />
           <Route path={"/regist"} component={Regist} />
           <Route path={"/project"} component={CreateProjects} />
+          <Route path={"/logout"} component={LogOut} />
         </Switch>
       </BrowserRouter>
     );
