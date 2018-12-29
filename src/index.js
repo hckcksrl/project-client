4144511041e00bd7094de7c10b2b114248490909
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-29 20:14:52 +0900

    Project List View APi

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import App from "./Components/App/index";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
 
+document.body.style.background = "rgb(0, 121, 191)";
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
