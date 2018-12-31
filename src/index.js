4cc321f389630a558170972cb2bbc4af8d6fa73a
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2018-12-31 20:04:20 +0900

    App Index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./Components/App/index";
+import Container from "./Components/App";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
 
 document.body.style.background = "rgb(0, 121, 191)";
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Container />
   </ApolloProvider>,
   document.getElementById("root")
 );
