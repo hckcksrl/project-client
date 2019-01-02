90f6f2e30ba4cb6fa8775012dd9374097e884ff1
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-02 18:13:13 +0900

    App Index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 import Container from "./Components/App";
 import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
+import "./style.css";
 
 document.body.style.background = "rgb(0, 121, 191)";
+// document.getElementById("root").style.height = "100%";
+// document.getElementById("root").style.width = "100%";
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Container />
