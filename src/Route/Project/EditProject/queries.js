222564b4a8a8282da9881d64254f1bed9e773136
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-14 17:36:47 +0900

    EditProject queries

---
+++ src/Route/Project/EditProject/queries.js
@@ -0,0 +1,10 @@
+import gql from "graphql-tag";
+
+export const EditProject = gql`
+  mutation Edit_Project($id: Int!, $projectname: String) {
+    EditProject(id: $id, projectname: $projectname) {
+      result
+      error
+    }
+  }
+`;
