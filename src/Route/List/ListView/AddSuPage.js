984a7f7131788736d0c80220fe81755b4037e87a
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-16 21:34:26 +0900

    AddSubPage List View

--- src/Route/List/ListView/AddSuPage.js
+++ src/Route/List/ListView/AddSuPage.js
@@ -1,29 +1,56 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const AddSub = styled.div`
   border-radius: 0 0 3px 3px;
   color: #6b808c;
-  display: block;
+  display: inline-block;
   flex: 0 0 auto;
-  padding: 8px;
+  padding: 10px;
   position: relative;
   text-decoration: none;
+  height: auto;
 `;
 
 const AddSubInner = styled.div`
   margin-left: 5px;
+  height: auto;
 `;
+
+const AddSubName = styled.a`
+  z-index: 5;
+`;
+
 class AddSubPage extends React.Component {
   render() {
+    const { projectid } = this.props;
     return (
-      <AddSub>
+      <AddSub
+        id={`addwrap${projectid}`}
+        onClick={() => this.click(projectid)}
+        on={() => this.focusout(projectid)}
+      >
         <AddSubInner>
-          <span>Add SubProject</span>
+          <AddSubName>Add SubProject</AddSubName>
         </AddSubInner>
       </AddSub>
     );
   }
+  click = id => {
+    document.getElementById(`area${id}`).style.display = "block";
+    document.getElementById(`addwrap${id}`).style.display = "none";
+    document.getElementById(`txtarea${id}`).focus();
+  };
+  focusout = id => {
+    document.getElementById(`addwrap${id}`).style.display = "block";
+    document.getElementById(`area${id}`).style.display = "none";
+    document.getElementById(`txtarea${id}`);
+  };
 }
+
+AddSubPage.propTypes = {
+  projectid: PropTypes.number.isRequired
+};
 
 export default AddSubPage;
