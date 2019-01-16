6fa23cf8e1a37bcb3b383d48e8f05ed80c08c919
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-16 21:35:12 +0900

    Delete Sub Project

---
+++ src/Route/SubProject/DeleteSubProject/DeleteSubProject.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { DeleteSub } from "./queries";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Mutation } from "react-apollo";
+import { GetList } from "../../List/queris";
+
+const Delete = styled.button`
+  background-color: transparent;
+  background-clip: padding-box;
+  background-origin: padding-box;
+  border-radius: 3px;
+  border: none;
+  padding: 4px;
+  position: absolute;
+  right: 2px;
+  top: 4px;
+  z-index: 40;
+  text-align: center;
+`;
+const DeleteDiv = styled.div``;
+const Form = styled.form``;
+class DeleteSubProject extends React.Component {
+  render() {
+    const { subprojectid, userid } = this.props;
+    return (
+      <DeleteDiv>
+        <Mutation mutation={DeleteSub}>
+          {Delete_Sub => (
+            <Form
+              onSubmit={e => {
+                e.preventDefault();
+                Delete_Sub({
+                  refetchQueries: [
+                    { query: GetList, variables: { id: userid } }
+                  ],
+                  variables: { id: subprojectid }
+                });
+              }}
+            >
+              <Delete id={`sub_del${subprojectid}`} type="submit">
+                삭제
+              </Delete>
+            </Form>
+          )}
+        </Mutation>
+      </DeleteDiv>
+    );
+  }
+}
+
+DeleteSubProject.propTypes = {
+  subprojectid: PropTypes.number.isRequired,
+  userid: PropTypes.number.isRequired
+};
+
+export default DeleteSubProject;
