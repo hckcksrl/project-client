7f1d97ab1bfc68e64186b38f1bf8e12740ca8f7b
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-17 19:49:05 +0900

    Delete Project

--- src/Route/Project/DeleteProject/DeleteProject.js
+++ src/Route/Project/DeleteProject/DeleteProject.js
@@ -1,15 +1,60 @@
-import React from "react"
-import {Mutation, Query} from "react-apollo"
-import {Delete_Project} from "./queries"
+import React from "react";
+import { Mutation } from "react-apollo";
+import { DelProject } from "./queries";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { GetList } from "../../List/queris";
 
+const Div = styled.div`
+  position: absolute;
+  right: 5px;
+  top: 10px;
+`;
+const Delete = styled.button`
+  font-size: 15px;
+  border: none;
+  background-color: transparent;
+`;
 
+const Form = styled.form``;
 
-export default class DeleteProject extends React.Component{
-    render(){
-        return(
-            <Query query={}>
-                            <Mutation mutation={Delete_Project}></Mutation>
-            </Query>
-        )
-    }
+class DeleteProject extends React.Component {
+  render() {
+    const { projectid } = this.props;
+    return (
+      <Div>
+        <Mutation mutation={DelProject}>
+          {Delete_Project => (
+            <Form onSubmit={e => this._delete(e, Delete_Project)}>
+              <Delete id={`p-del${projectid}`} type="submit">
+                삭제
+              </Delete>
+            </Form>
+          )}
+        </Mutation>
+      </Div>
+    );
+  }
+  _delete = (e, Delete_Project) => {
+    const { projectid, userid } = this.props;
+    e.preventDefault();
+    Delete_Project({
+      refetchQueries: [
+        {
+          query: GetList,
+          variables: { id: userid }
+        }
+      ],
+      variables: {
+        id: projectid
+      }
+    });
+  };
 }
+
+DeleteProject.propTypes = {
+  projectid: PropTypes.number.isRequired,
+  userid: PropTypes.number.isRequired
+};
+
+export default DeleteProject;
