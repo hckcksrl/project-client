8f2dc3c306af92108c104bffe74f74305f547305
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-21 19:34:24 +0900

    Index

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { ApolloProvider } from "react-apollo";
 import client from "./apollo";
 import "./style.css";
 
-document.body.style.background = "rgb(0, 121, 191)";
 ReactDOM.render(
   <ApolloProvider client={client}>
     <Container />
