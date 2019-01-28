13114b6761586bde97410fe99c7b97048efe94a0
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-28 19:48:38 +0900

    Edit Project

--- src/Route/Project/EditProject/EditProject.js
+++ src/Route/Project/EditProject/EditProject.js
@@ -3,65 +3,15 @@ import { Mutation } from "react-apollo";
 import { EditProject } from "./queries";
 import PropTypes from "prop-types";
 import { GetList } from "../../List/queris";
-import styled from "styled-components";
 import TextareaAutosize from "react-textarea-autosize";
-
-const ProjectEdit = styled(TextareaAutosize)`
-  background: ${props => props.inputback};
-  box-sizing: border-box;
-  border: 1px solid transparent;
-  border-radius: 3px;
-  top: 7px;
-  max-height: 256px;
-  padding-left: 6px;
-  width: 85%;
-  position: absolute;
-  left: 8px;
-  font-size: 20px;
-  line-height: 20px;
-  text-align: start;
-  visibility: ${props => props.inputvisible};
-  resize: none;
-  white-space: pre-line;
-  z-index: 1;
-`;
-const Form = styled.form`
-  height: 100%;
-  width: 100%;
-`;
-
-const Button = styled.button`
-  visibility: hidden;
-  float: right;
-  z-index: 10;
-  top: 7px;
-`;
-
-const ProjectName = styled.a`
-  padding-left: 6px;
-  font-size: 20px;
-  width: 100%;
-  height: 100%;
-  line-height: 14px;
-  white-space: pre-wrap;
-  line-height: 20px;
-  float: left;
-  padding-top: 7px;
-`;
-const ProjectContainer = styled.div`
-  cursor: pointer;
-  height: 100%;
-  width: 95%;
-  visibility: ${props => props.visible};
-`;
+import "./EditProject.scss";
 
 class Edit extends React.Component {
   constructor() {
     super();
     this.state = {
-      e_back: "transparent",
-      e_visible: "hidden",
-      p_visible: "visible"
+      name_class: "project-name",
+      edit_class: "project-edit"
     };
   }
   render() {
@@ -69,48 +19,38 @@ class Edit extends React.Component {
     return (
       <Mutation mutation={EditProject}>
         {Edit_Project => (
-          <Form
-            id={`form${id}`}
-            onSubmit={e => this._complete(e, id, Edit_Project, userid)}
+          <div
+            className="project-container"
+            id={`project${id}`}
+            onClick={e => this._click(e, id)}
           >
-            <ProjectContainer
-              id={`project${id}`}
-              onClick={() => this._click(id, projectname)}
-              visible={this.state.p_visible}
-            >
-              <ProjectName id={`name${id}`}>{projectname}</ProjectName>
-              <ProjectEdit
-                defaultValue={projectname}
-                id={`edit${id}`}
-                onKeyDown={e => this._press(e, id)}
-                onKeyUpCapture={e => this._height(e, id)}
-                onBlurCapture={e => this._focusout(e, id, projectname)}
-                inputback={this.state.e_back}
-                inputvisible={this.state.e_visible}
-              />
-              <Button id={`button${id}`} type="submit">
-                확인
-              </Button>
-            </ProjectContainer>
-          </Form>
+            <a className={this.state.name_class}>{projectname}</a>
+            <TextareaAutosize
+              className={this.state.edit_class}
+              defaultValue={projectname}
+              id={`edit${id}`}
+              onKeyDown={e => this._press(e, id, Edit_Project, userid)}
+              onBlurCapture={e => this._focusout(e)}
+              onSubmit={e => this._complete(e, id, Edit_Project, userid)}
+            />
+          </div>
         )}
       </Mutation>
     );
   }
-  _click = (key, projectname) => {
+  _click = (e, id) => {
     this.setState({
-      p_visible: "hidden",
-      e_back: "white",
-      e_visible: "visible"
+      name_class: "project-name hidden",
+      edit_class: "project-edit edit"
     });
     window.setTimeout(() => {
-      document.getElementById(`edit${key}`).focus();
+      document.getElementById(`edit${id}`).focus();
     });
     return true;
   };
   _complete = (e, id, Edit_Project, userid) => {
     e.preventDefault();
-    const project = document.getElementById(`edit${id}`).value;
+    const project = e.target.value;
     Edit_Project({
       refetchQueries: [
         {
@@ -124,49 +64,29 @@ class Edit extends React.Component {
       }
     });
   };
-  _press = (e, key) => {
+  _press = (e, id, Edit_Project, userid) => {
     const code = e.which;
-    const edit = document.getElementById(`edit${key}`);
-    const height = parseInt(edit.style.height) + 10 + "px";
-    document.getElementById(`main${key}`).style.height = height;
     if (code === 13) {
-      if (edit.value === "") {
-        this.setState({
-          p_visible: "visible",
-          e_back: "transparent",
-          e_visible: "hidden"
-        });
+      if (e.target.value === "") {
         return true;
       } else {
         e.preventDefault();
-        document.getElementById(`button${key}`).click();
+        this._complete(e, id, Edit_Project, userid);
         this.setState({
-          p_visible: "visible",
-          e_back: "transparent",
-          e_visible: "hidden"
+          name_class: "project-name",
+          edit_class: "project-edit"
         });
+        this._focusout(e);
         return true;
       }
     }
     return true;
   };
-
-  _height = (e, key) => {
-    e.preventDefault();
-    const height = parseInt(e.target.style.height) + 10 + "px";
-    document.getElementById(`main${key}`).style.height = height;
-  };
-
-  _focusout = (e, key, projectname) => {
+  _focusout = e => {
     e.preventDefault();
     this.setState({
-      p_visible: "visible",
-      e_back: "transparent",
-      e_visible: "hidden"
-    });
-    const height = parseInt(e.target.style.height) + 10 + "px";
-    window.setTimeout(() => {
-      document.getElementById(`main${key}`).style.height = height;
+      name_class: "project-name",
+      edit_class: "project-edit"
     });
   };
 }
