6c5ddfd4280e2bdfffb24ee54f5f6a68ade7d5f9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-28 19:48:24 +0900

    Delete Project

--- src/Route/Project/DeleteProject/DeleteProject.js
+++ src/Route/Project/DeleteProject/DeleteProject.js
@@ -1,39 +1,28 @@
 import React from "react";
 import { Mutation } from "react-apollo";
 import { DelProject } from "./queries";
-import styled from "styled-components";
 import PropTypes from "prop-types";
 import { GetList } from "../../List/queris";
-
-const Div = styled.div`
-  position: absolute;
-  right: 5px;
-  top: 10px;
-`;
-const Delete = styled.button`
-  font-size: 15px;
-  border: none;
-  background-color: transparent;
-  cursor: pointer;
-`;
-
-const Form = styled.form``;
+import "./DeleteProject.scss";
 
 class DeleteProject extends React.Component {
   render() {
     const { projectid } = this.props;
     return (
-      <Div>
+      <div className="delete-div">
         <Mutation mutation={DelProject}>
           {Delete_Project => (
-            <Form onSubmit={e => this._delete(e, Delete_Project)}>
-              <Delete id={`p-del${projectid}`} type="submit">
-                삭제
-              </Delete>
-            </Form>
+            <button
+              className="delete"
+              onClick={e => this._delete(e, Delete_Project)}
+              id={`p-del${projectid}`}
+              type="submit"
+            >
+              삭제
+            </button>
           )}
         </Mutation>
-      </Div>
+      </div>
     );
   }
   _delete = (e, Delete_Project) => {
