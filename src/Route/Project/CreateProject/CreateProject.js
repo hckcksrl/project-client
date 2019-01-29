52da88874db0e5414aa244517be7b4026f8cc67f
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-29 21:05:51 +0900

    Create Project

--- src/Route/Project/CreateProject/CreateProject.js
+++ src/Route/Project/CreateProject/CreateProject.js
@@ -1,100 +1,67 @@
 import React from "react";
-import styled from "styled-components";
 import { Mutation } from "react-apollo";
 import { CreateProject } from "./queries";
 import { GetList } from "../../List/queris";
 import PropTypes from "prop-types";
-
-const Div = styled.div`
-  flex: auto;
-  background-color: #dfe3e6;
-  border-radius: 3px;
-  box-sizing: border-box;
-  display: none;
-  height: 100%;
-  flex-direction: column;
-  position: relative;
-`;
-
-const Form = styled.form`
-  height: 100%;
-  width: 100%;
-`;
-
-const Div2 = styled.div`
-  width: 100%;
-  height: 50%;
-`;
-const Div3 = styled.div`
-  width: 100%;
-  height: 50%;
-`;
-
-const AddArea = styled.input`
-  background: white;
-  box-sizing: border-box;
-  border: 1px solid transparent;
-  border-radius: 3px;
-  top: 7px;
-  max-height: 256px;
-  padding-left: 6px;
-  width: 95.5%;
-  position: absolute;
-  left: 8px;
-  font-size: 15px;
-  line-height: 20px;
-  text-align: start;
-  resize: none;
-  white-space: pre-line;
-  z-index: 1;
-`;
-
-const Button = styled.button`
-  background-color: green;
-  box-shadow: 0 1px 0 0 #3f6f21;
-  border: none;
-  color: #fff;
-  cursor: pointer;
-  display: inline-block;
-  font-weight: 700;
-  font-size: 15px;
-  padding: 6px 12px;
-  text-align: center;
-  margin-left: 8px;
-  top: 7px;
-`;
+import "./CreateProject.scss";
 
 class CreateProjects extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      add_project: "add-project-wrap"
+    };
+  }
   render() {
-    const { userid } = this.props;
     return (
-      <Div id={`add_div`}>
-        <Mutation mutation={CreateProject}>
-          {Create_Project => (
-            <Form onSubmit={e => this._Create(e, userid, Create_Project)}>
-              <Div2 id={`c-p-wrap`}>
-                <AddArea
-                  type="text"
-                  id={`addArea`}
-                  onKeyDown={e => this._press(e)}
-                  placeholder="Enter Create Project"
-                  onBlur={e => this._focusout(e)}
-                />
-              </Div2>
-              <Div3>
-                <Button id={`c_p_btn`} type="submit">
-                  Create Project
-                </Button>
-              </Div3>
-            </Form>
-          )}
-        </Mutation>
-      </Div>
+      <div className={this.state.add_project} id="add">
+        <div
+          className="click-add"
+          id={`ClickAdd`}
+          onClick={e => this._Click(e)}
+        >
+          <span>Add Project</span>
+        </div>
+        <div className="add-project-div-wrap">
+          <div className="ceate-project-wrap">
+            <Mutation mutation={CreateProject}>
+              {Create_Project => (
+                <div className="create-wrap">
+                  <input
+                    id="create-project"
+                    type="text"
+                    onKeyDown={e => this._press(e)}
+                    placeholder="Enter Create Project"
+                    onBlur={e => this._focusout(e)}
+                  />
+                  <button
+                    id="create-project-btn"
+                    onClick={e => this._Create(e, Create_Project)}
+                    type="submit"
+                  >
+                    Create Project
+                  </button>
+                </div>
+              )}
+            </Mutation>
+          </div>
+        </div>
+      </div>
     );
   }
-  _Create = (e, userid, Create_Project) => {
+  _Click = e => {
+    this.setState({
+      add_project: "add-project-wrap click"
+    });
+    window.setTimeout(() => {
+      document.getElementById("create-project").focus();
+    });
+  };
+  _Create = (e, Create_Project) => {
+    console.log("a");
     e.preventDefault();
-    const project = document.getElementById(`addArea`).value;
+    const { userid } = this.props;
+    const project = document.getElementById("create-project").value;
     Create_Project({
       refetchQueries: [
         {
@@ -109,34 +76,35 @@ class CreateProjects extends React.Component {
   };
   _press = e => {
     const code = e.which;
+    const data = e.target.value.trim();
     if (code === 13) {
       e.preventDefault();
-      if (document.getElementById(`addArea`).value === "") {
-        document.getElementById(`add_div`).style.display = "none";
-        document.getElementById(`ClickAdd`).style.display = "none";
-
+      if (data === "") {
+        this.setState({
+          add_project: "add-project-wrap"
+        });
+        e.target.value = "";
         return false;
       } else {
-        document.getElementById(`c_p_btn`).click();
-        document.getElementById(`add_div`).style.display = "none";
-        document.getElementById(`ClickAdd`).style.display = "none";
+        document.getElementById("create-project-btn").click();
+        this.setState({
+          add_project: "add-project-wrap"
+        });
         e.target.value = "";
         return true;
       }
     }
     return true;
   };
-
   _focusout = e => {
     e.preventDefault();
-    document.getElementById(`ClickAdd`).style.display = "block";
-    document.getElementById(`add_div`).style.display = "none";
-    document.getElementById(`add`).style.height = "30px";
-    document.getElementById(`addArea`).value = "";
+    this.setState({
+      add_project: "add-project-wrap"
+    });
+    e.target.value = "";
   };
 }
-
-CreateProject.propTypes = {
+CreateProjects.propTypes = {
   userid: PropTypes.number.isRequired
 };
 
