38e42cf0538b715d29006ed9ecde1eb3966c17aa
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-01-30 20:26:24 +0900

    Footer

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./index.scss";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+        <p>© Copyright 2019 hckcksrl</p>
+        <p>프로젝트 관리</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
