eb5aead505d7bd5b214ccaf7d2515a3976816555
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-01 22:41:52 +0900

    SubProject View

--- src/Route/List/ListView/SubProject/SubProject.js
+++ src/Route/List/ListView/SubProject/SubProject.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import DeleteSubProject from "../../../SubProject/DeleteSubProject";
 import "./SubProject.scss";
+import CreateDetail from "../../../DetailProject/CreateDetailProject";
 
 class SubProjectPage extends React.Component {
   constructor() {
@@ -16,11 +17,11 @@ class SubProjectPage extends React.Component {
       <div className="subproject">
         {subproject.map(data => {
           return (
-            <a
+            <div
               className={this.state.sub_main_class}
               key={data.id}
               id={`sub_div${data.id}`}
-              href="/aaa"
+              onClick={e => this._click(e, data.detaillist)}
             >
               <div className="subproject-div">
                 <span className="subproject-name" id={`sub${data.id}`}>
@@ -28,14 +29,21 @@ class SubProjectPage extends React.Component {
                 </span>
               </div>
               <DeleteSubProject subprojectid={data.id} userid={userid} />
-            </a>
+            </div>
           );
         })}
       </div>
     );
   }
+  _click = (e, detail) => {
+    const position = e.target.getBoundingClientRect();
+    const pop_up = document.getElementsByClassName("pop-up-wrap")[0];
+    document.getElementsByClassName("pop-main")[0].style.display = "block";
+    pop_up.style.left = `${position.left}px`;
+    pop_up.style.top = `${position.top}px`;
+    CreateDetail(detail);
+  };
 }
-
 SubProjectPage.propTypes = {
   subproject: PropTypes.array.isRequired,
   userid: PropTypes.number.isRequired
