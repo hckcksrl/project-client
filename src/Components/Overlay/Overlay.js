cd4d254ae3e5114d55e84f8f9a8ae5e2775408bf
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-01 22:41:21 +0900

    OverLay View

--- src/Components/Overlay/Overlay.js
+++ src/Components/Overlay/Overlay.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Overlay.scss";
+import ReactDOM from "react-dom";
 
 const Overlay = () => {
   return (
-    <div className="overlay">
-      <div>a</div>
+    <div className="pop-main" onClick={e => FocusOut(e)}>
+      <div className="pop-up-wrap" id="pop">
+        <div className="pop-up">
+          <div className="focus-dummy" />
+          <div className="details" />
+        </div>
+      </div>
     </div>
   );
+};
+const FocusOut = e => {
+  if (e.target === document.getElementsByClassName("pop-main")[0]) {
+    const element = null;
+    ReactDOM.render(element, document.getElementsByClassName("details")[0]);
+    document.getElementsByClassName("pop-main")[0].style.display = "none";
+  } else {
+    return true;
+  }
 };
 
 export default Overlay;
