1e1a851f24b887b20d97a998dc811c2a9ad13bb9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-08 17:54:42 +0900

    Login

--- src/Route/User/Login/Login.js
+++ src/Route/User/Login/Login.js
@@ -1,51 +1,28 @@
 import React from "react";
-import styled from "styled-components";
+import PropTypes from "prop-types";
 import { EmailLogin } from "./queries";
 import { Mutation } from "react-apollo";
 import { Logined } from "../../../LocalQueries";
 
-const Input = styled.input`
-  padding: 1em;
-  margin: 1em;
-  color: "black";
-  background: papayawhip;
-  border: none;
-  border-radius: 3px;
-`;
+class Login extends React.Component {
+  submit = (e, Email_Login) => {
+    const { email, password } = this.props;
+    e.preventDefault();
+    Email_Login({
+      variables: {
+        email: email,
+        password: password
+      }
+    });
+  };
 
-const Password = styled.input`
-  padding: 1em;
-  margin: 1em;
-  color: "black";
-  background: papayawhip;
-  border: none;
-  border-radius: 3px;
-`;
-const Button = styled.button`
-  font-size: 18px;
-  line-height: 30px;
-  font-weight: 600;
-`;
-
-const Form = styled.form``;
-
-export default class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: ""
-    };
-  }
   render() {
     const { history } = this.props;
-    const { email, password } = this.state;
     return (
       <Mutation mutation={Logined}>
         {UserLogin => (
           <Mutation
             mutation={EmailLogin}
-            variables={{ email: email, password: password }}
             onCompleted={data => {
               const { Login } = data;
               if (Login.result) {
@@ -57,50 +34,30 @@ export default class Login extends React.Component {
                 });
                 history.go();
               } else {
-                console.log(Login.error);
+                return Login.error;
               }
             }}
           >
             {Email_Login => (
-              <Form
+              <button
+                type="submit"
                 onSubmit={e => {
-                  e.preventDefault();
-                  Email_Login({
-                    variables: {
-                      email: email,
-                      password: password
-                    }
-                  });
+                  this.submit(e, Email_Login);
                 }}
               >
-                <Input
-                  placeholder="email"
-                  type="email"
-                  onChange={this._onInputChange}
-                  value={email}
-                  name={"email"}
-                />
-                <Password
-                  placeholder="password"
-                  type="password"
-                  onChange={this._onInputChange}
-                  value={password}
-                  name={"password"}
-                />
-                <Button type="submit">Login</Button>
-              </Form>
+                Login
+              </button>
             )}
           </Mutation>
         )}
       </Mutation>
     );
   }
-  _onInputChange = event => {
-    const {
-      target: { value, name }
-    } = event;
-    this.setState({
-      [name]: value
-    });
-  };
 }
+
+Login.propTypes = {
+  email: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
+};
+
+export default Login;
