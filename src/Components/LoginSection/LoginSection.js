47546915edc65c4811879993b0f25064afae2293
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-08 17:52:20 +0900

    LoginSection

--- src/Components/LoginSection/LoginSection.js
+++ src/Components/LoginSection/LoginSection.js
@@ -6,23 +6,25 @@ import Footer from "../Footer";
 import Header from "../Header";
 import "./index.scss";
 import Overlay from "../Overlay/Overlay";
+// import LoginForm from "../LoginForm";
 
 const LoginSection = () => {
   return (
     <div className="body">
+      <script src="https://unpkg.com/ionicons@4.2.2/dist/ionicons.js" />
       <div className="content">
         <Header />
         <section className="section">
           <Switch>
             <Route exact={true} path={"/"} component={Home} />
             <Route path={"/list/:userid"} component={List} />
-            {/* <Route path={"/:subproject"} component={Footer} /> */}
             <Redirect to={"/"} />
           </Switch>
         </section>
         <Footer />
       </div>
       <Overlay />
+      <div className="login-sign">{/* <LoginForm /> */}</div>
     </div>
   );
 };
