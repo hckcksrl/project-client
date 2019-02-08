7fd6d83b0451135e1f1b29a62b0ecfd5e29d29b6
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-08 17:52:10 +0900

    Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,32 +1,47 @@
-import React from "react";
-import { Query } from "react-apollo";
-import { GetMy } from "./queries";
+import React, { Component } from "react";
 import "./Header.scss";
-import LogOut from "../../Route/User/Logout";
 
-const Header = () => (
-  <Query query={GetMy}>
-    {({ loading, data, error }) => {
-      if (loading) return "loading";
-      if (error) return "something happened";
-      return (
-        <div className="Header">
-          <nav className="Header-nav">
-            <div className="Header-height">
-              <div className="Header-center">
-                <div className="Header-wrap">
-                  <span className="Name">{data.GetMy.user.email}</span>
-                </div>
-                <div>
-                  <LogOut />
-                </div>
+class Header extends Component {
+  // loginClick = e => {
+  //   const position = e.target.getBoundingClientRect();
+  //   const pop_up = document.getElementsByClassName("details")[0];
+  //   document.getElementsByClassName("pop-main")[0].style.display = "block";
+  //   pop_up.style.left = `${position.left}px`;
+  //   pop_up.style.top = `${position.top}px`;
+  // };
+
+  // signClick = e => {
+  //   const position = e.target.getBoundingClientRect();
+  // };
+
+  render() {
+    return (
+      <div className="Header">
+        <div className="Header-height">
+          <div className="Header-center">
+            <div className="Header-wrap">
+              <span className="Name">PM</span>
+            </div>
+            <div className="border" />
+            <div className="login-logout">
+              <div
+                className="login-signup"
+                // onClick={e => this.loginClick(e)}
+              >
+                Log in
+              </div>
+              <div
+                className="login-signup"
+                // onClick={e => this.signClick(e)}
+              >
+                Sign Up
               </div>
             </div>
-          </nav>
+          </div>
         </div>
-      );
-    }}
-  </Query>
-);
+      </div>
+    );
+  }
+}
 
 export default Header;
