ca83a8a3ed1192c8fbdcd642176487fccb66e546
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-11 16:16:53 +0900

    Home View

--- src/Route/Home/Home.js
+++ src/Route/Home/Home.js
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import { MyId } from "./queries";
 import { Query } from "react-apollo";
-import styled from "styled-components";
+import PropTypes from "prop-types";
+import back from "./back.png";
+import "./Home.scss";
 
-const Button = styled.button`
-  font-size: 18px;
-  line-height: 30px;
-  font-weight: 600;
-`;
-
-const Form = styled.form``;
-
-export default class Home extends React.Component {
+class Home extends Component {
   render() {
-    const { history } = this.props;
+    const { isLoggedIn } = this.props;
+    console.log(this.props);
     return (
-      <Query query={MyId}>
-        {data => {
-          return (
-            <Form
-              onSubmit={e => {
-                history.push(`/list/${data.data.GetMy.user.id}`);
-              }}
-            >
-              <Button type="submit">List</Button>
-            </Form>
-          );
-        }}
-      </Query>
+      <div className="home-back">
+        <img src={back} className="home-img" alt="Home" />
+        {isLoggedIn ? (
+          <Query query={MyId}>
+            {({ loading, error, data }) => {
+              if (loading) return "loading";
+              if (error) return "error";
+              return (
+                <div className="home-form">
+                  <span
+                    className="home-button"
+                    onClick={() => this.submit(data.GetMy.user.id)}
+                  >
+                    My Project
+                  </span>
+                </div>
+              );
+            }}
+          </Query>
+        ) : null}
+      </div>
     );
   }
+  submit = id => {
+    const { history } = this.props;
+    history.push(`/list/${id}`);
+  };
 }
+
+Home.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+export default Home;
