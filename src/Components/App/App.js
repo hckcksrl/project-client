416246dad39938aea438239c46feebd97f4acbe9
hckcksrl@gmail.com <hckcksrl@hckcksrlui-MacBook-Pro.local>
2019-02-11 16:15:33 +0900

    App Route

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,11 +3,32 @@ import { BrowserRouter as Router } from "react-router-dom";
 import PropTypes from "prop-types";
 import LoginSection from "../LoginSection";
 import LogoutSection from "../LogoutSection";
+import Footer from "../Footer";
+import Header from "../Header";
+import Overlay from "../Overlay";
+import "./index.scss";
 
 class App extends React.Component {
   render() {
     const { isLoggedIn } = this.props;
-    return <Router>{isLoggedIn ? <LoginSection /> : <LogoutSection />}</Router>;
+    return (
+      <Router>
+        <div className="body">
+          <div className="content">
+            <Header isLoggedIn={isLoggedIn} />
+            <section className="section">
+              {isLoggedIn ? (
+                <LoginSection isLoggedIn={isLoggedIn} />
+              ) : (
+                <LogoutSection isLoggedIn={isLoggedIn} />
+              )}
+            </section>
+            <Footer />
+          </div>
+          <Overlay />
+        </div>
+      </Router>
+    );
   }
 }
 
